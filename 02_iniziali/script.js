fc391af7bb9050e5076a3e739496646ed5476482
oumouniaonedabre-dot <oumouniaonedabre@gmail.com>
2026-05-21 00:06:27 +0200

    corretto

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -1,23 +1,18 @@
 /* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
 
-const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
+const nomi = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 // Dichiara la funzione qui.
-function getInitials(arr) {
-    const initials = [];
-    for (let i = 0; i < arr.length; i++) {
-        const name = arr[i];
-        const initial = name.charAt(0);
-        initials.push(initial);
-    }
-    return initials;
+function prendiIniziali(listaNomi) {
+    // .map prende ogni "singoloNome" dalla "listaNomi" e ne estrae la prima lettera
+    return listaNomi.map(singoloNome => singoloNome.charAt(0));
 }
 
 
 // Invoca la funzione qui e stampa il risultato in console
-const initialsArray = getInitials(names);
-console.log(initialsArray);
+const arrayIniziali = prendiIniziali(nomi);
+console.log(arrayIniziali);
 
 
-//Risultato atteso: ["A", "L", "M", "A", "G", "A"]
+// Risultato atteso: ["A", "L", "M", "A", "G", "A"]
